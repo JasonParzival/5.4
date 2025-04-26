e6bf7049d583a2c2ecf863ae65a50497eea2090d
JasonParzival <ruslanmuhamed123@gmail.com>
2025-04-26 14:05:12 +0800

    Разобрался с логикой

--- controllers/SearchController.php
+++ controllers/SearchController.php
@@ -10,17 +10,33 @@ class SearchController extends BasePortalTwigController {
 
         $type = isset($_GET['type']) ? $_GET['type'] : '';
         $title = isset($_GET['title']) ? $_GET['title'] : '';
+        $info = isset($_GET['info']) ? $_GET['info'] : '';
 
-        $sql = <<<EOL
+        if($type == ''){
+            $sql = <<<EOL
+SELECT id, title
+FROM portal_characters
+WHERE (:title = '' OR title like CONCAT('%', :title, '%'))
+        AND (:info = '' OR info like CONCAT('%', :info, '%'))
+EOL;
+            $query = $this->pdo->prepare($sql);
+            $query->bindValue("title", $title);
+            $query->bindValue("info", $info);
+        } else {
+            $sql = <<<EOL
 SELECT id, title
 FROM portal_characters
 WHERE (:title = '' OR title like CONCAT('%', :title, '%'))
         AND (type = :type)
+        AND (:info = '' OR info like CONCAT('%', :info, '%'))
 EOL;
+            $query = $this->pdo->prepare($sql);
+            $query->bindValue("title", $title);
+            $query->bindValue("type", $type);
+            $query->bindValue("info", $info);
+        }
 
-        $query = $this->pdo->prepare($sql);
-        $query->bindValue("title", $title);
-        $query->bindValue("type", $type);
+        
         $query->execute(); 
 
         $context['title_objects'] = $query->fetchAll();
